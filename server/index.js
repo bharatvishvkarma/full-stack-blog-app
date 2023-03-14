fe9c302515e470e1a689c4e49cd57d56d4247730
bharatvishvkarma <bharatsharma1995@outlook.com>
2023-03-14 22:55:22 +0530

    all keys secure

--- server/index.js
+++ server/index.js
@@ -10,15 +10,8 @@ require('dotenv').config();
 
 app.use(cors())
 app.use(express.json());
-// Increase the maximum payload limit to 50MB
 
-// app.use( bodyParser.json({limit: '50mb'}) );
-// app.use(bodyParser.urlencoded({
-//   limit: '50mb',
-//   extended: true,
-//   parameterLimit:50000
-// }));
-
+   
 
 app.use('/', authRouter)
 const port = 7777
